9e64bb265b02759a23fb46612dca8c0f0bbe57db
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-02-16 21:06:20 -0600

    Finished Ch.11 - Pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -11,6 +11,7 @@ export default async function Page({ searchParams }: { searchParams?: { query?: 
 
   const query = searchParams?.query || ""
   const currentPage = Number(searchParams?.page) || 1
+  const totalPages = await fetchInvoicesPages(query)
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -24,7 +25,7 @@ export default async function Page({ searchParams }: { searchParams?: { query?: 
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
